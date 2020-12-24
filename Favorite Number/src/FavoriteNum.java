9e06026f339d5b57e659199ef706df4ec344be1c
SURAJ <SURAJ@DESKTOP-TOIRQI8>
2020-12-24 12:07:59 +0530

    file upload

---
+++ Favorite Number/src/FavoriteNum.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class FavoriteNum {
+
+	public static void main(String[] args) {
+	
+		System.out.print("Enter your Fav Number");
+		
+		Scanner input = new Scanner(System.in);
+		
+		int i = input.nextInt();
+		
+		
+		System.out.println(i+"  is my fav number too");
+		
+		
+
+	}
+
+}
