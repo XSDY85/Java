16b444dc07d20e96e78f060754e6c0273c971d99
SURAJ <SURAJ@DESKTOP-TOIRQI8>
2020-12-25 11:51:21 +0530

    upload

---
+++ If Else Even Odd/src/EvenOdd.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class EvenOdd {
+
+	public static void main(String[] args) {
+		
+		Scanner i = new Scanner(System.in);
+		
+		System.out.println("Please enter a number");
+		
+		int n = i.nextInt();
+		
+		if(n%2==0)
+		{
+			System.out.println(n + " is Even");
+		}
+		
+		else
+		{
+			System.out.println(n + " is Odd");
+			
+		}
+	}
+
+}
