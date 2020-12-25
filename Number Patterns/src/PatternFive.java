5e1440b61a84a8ea2a6fe7e16b8764538959fd46
SURAJ <SURAJ@DESKTOP-TOIRQI8>
2020-12-25 11:58:57 +0530

    upload

---
+++ Number Patterns/src/PatternFive.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class PatternFive {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner s = new Scanner (System.in);
+		System.out.println(" Enter a number ");
+		int n = s.nextInt();
+		
+		for ( int i=1; i<=n; i++)
+		{
+			if (i==1 || i==n)
+			{
+				for ( int j= 1; j<=n; j++)
+				{
+					
+				
+					System.out.print("*");
+				}
+			}
+	        
+			else 
+			{
+			for ( int j= 1; j<=n; j++)
+			{
+			     if (j==1 || j==n)
+			     	{
+				
+			    	 		System.out.print("*");
+			     	}
+			
+			     else
+			     	{
+			    	 	System.out.print(" ");
+			     	}    
+			     
+			}
+			}
+			System.out.println();
+		}
+	}
+
+}
---
+++ Number Patterns/src/PatternThree.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class PatternThree {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner s = new Scanner (System.in);
+		System.out.println(" Enter a number ");
+		int n = s.nextInt();
+		
+		for ( int i=1; i<=n; i++)
+		{
+			for (int j=1; j<=n-i; j++)
+			{
+				System.out.print(" ");
+			}
+			
+			for (int k=1; k<=2*i-1; k++)
+			{
+				System.out.print("*");
+			}
+		
+			System.out.println();
+		
+		}
+		
+	}
+
+}
