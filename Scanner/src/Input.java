76125d6434d619e2b36e79c3c3ec5c7130719e48
SURAJ <SURAJ@DESKTOP-TOIRQI8>
2020-12-25 12:01:28 +0530

    upload

---
+++ Scanner/src/Input.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Input {
+
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+
+		System.out.println("Enter your name");
+		System.out.println("My name is:  "+ input.nextLine());
+		
+		
+	}
+
+}
+
